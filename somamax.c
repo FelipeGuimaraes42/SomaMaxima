fad3bbf8791a1f723b94066dda98170b579012c0
Felipe Guimarães <felipeguima4@gmail.com>
2019-10-26 14:52:18 -0300

    Comentários

--- somamax.c
+++ somamax.c
@@ -3,17 +3,17 @@
 #define MAX 20
 
 int somamax(short int n,  int *vet, short int *ini, short int *fim){
+    //max_final é o valor a ser retornado e max_atual é a soma total daquele laço
     int max_final= 0;
     int max_atual;
     short int i, j;
-    short int cont= 0;
     for(i=0; i<n; i++){
         max_atual= 0;
         for(j=i; j<n; j++){
+            //Se os índices da soma máxima são os extremos, não há necessidade de calcular outras somas
             if((*ini==0) && (*fim==n-1))
                 break;
             max_atual+= vet[j];
-            cont++;
             if(max_atual>max_final){
                 max_final= max_atual;
                 *ini= i;
@@ -22,13 +22,13 @@ int somamax(short int n,  int *vet, short int *ini, short int *fim){
         }
         
     }
-    printf("Somas: %d\n", cont);
     return max_final;
 }
 
 int main(){
     short int n, i, indice_inical, indice_final;
     int max_final;
+    //Faz a validação da entrada
     do{
         printf("Insira o tamanho do vetor, entre %d e %d: ", MIN, MAX);
         scanf("%hi", &n);
